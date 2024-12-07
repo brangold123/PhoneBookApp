8c73b4b0ce81652b4f4bc7067c91eaf6e4d2f5c1
brangold123 <brandon.goldstein123@gmail.com>
2024-12-06 21:43:11 -0500

    Update PhoneBook.java
    Made phone number a string

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,4 +1,4 @@
-// Exercise 1
+
 import java.util.Scanner;
 
 public class PhoneBook {
@@ -9,13 +9,13 @@ public class PhoneBook {
 		System.out.println("Welcome to Brandon's, Jackson's, and Vincent's Car Management System!");
 		System.out.println();
 		
-		PhoneBookEntry entry1 = new PhoneBookEntry(5,"John","Smith","jsmith@gmail.edu",20037,2023334444);
+		PhoneBookEntry entry1 = new PhoneBookEntry(5,"John","Smith","jsmith@gmail.edu",20037,"2023334444");
 		
-		PhoneBookEntry entry2 = new PhoneBookEntry("James",202344344);
+		PhoneBookEntry entry2 = new PhoneBookEntry("James","202344344");
 		
 		PhoneBookEntry entry3 = new PhoneBookEntry("Sarah");
 		
-		entry1.setPhonenumber(202555555);
+		entry1.setPhonenumber("202555555");
 		entry1.printBookEntry();
 		
 		entry2.setZipcode(entry1.getZipcode());
